7500ff1519c3dd671ce27beb7ab80c897935850c
NiicoSnow <nicolas.lefebvre@student.heaj.be>
2026-05-16 18:21:36 +0200

    cards page bases

---
+++ src/components/Cards.tsx
@@ -0,0 +1,55 @@
+export function Cards() {
+    return (
+        <div className="cards grid">
+            <h2>Cartes</h2>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card1.jpg" alt="" />
+                    <h3>Card 1</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card2.jpg" alt="" />
+                    <h3>Card 2</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card3.jpg" alt="" />
+                    <h3>Card 3</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card4.jpg" alt="" />
+                    <h3>Card 4</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card5.jpg" alt="" />
+                    <h3>Card 5</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card6.jpg" alt="" />
+                    <h3>Card 6</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card7.jpg" alt="" />
+                    <h3>Card 7</h3>
+                </div>
+            </div>
+            <div className="cards__element">
+                <div className="cards__element--img">
+                    <img src="/card8.jpg" alt="" />
+                    <h3>Card 8</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,10 +1,11 @@
+import { Cards } from '../components/cards'
+
 export function CardsPage() {
     return (
       <section className="center">
         <div>
-          <h1>Cartes</h1>
-          <p>La page de cartes viendra ici.</p>
-        </div>
+            <Cards />
+          </div>
       </section>
     )
   }
